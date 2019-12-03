4009fab6847f803cd420a6d8163f45a8f45b7197
Vmp309 <52867523+Vmp309@users.noreply.github.com>
2019-12-02 21:19:14 -0300

    Update Data.h

--- Exercicio1.2/Data.h
+++ Exercicio1.2/Data.h
@@ -1,19 +1,24 @@
 #pragma once
 #include <iostream>
 
-
 class Data
 {
 
-public:
+private:
 	int dia;
 	int mes;
 	int ano;
 
-	void dataValida(int dia, int mes, int ano);
+public:
+	void setData(int dia_para, int mes_para, int ano_para);
+
+	bool dataValida(int dia, int mes, int ano);
 
 	void avancarDia(int dia, int mes, int ano);
 
+	int getDia();
+	int getMes();
+	int getAno();
 };
 
 
